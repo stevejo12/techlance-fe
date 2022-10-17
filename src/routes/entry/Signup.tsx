23295b2c3b83b0d6dd332bdfef632c82515e69d9
stevejo12 <37164804+stevejo12@users.noreply.github.com>
2022-10-17 22:58:56 +0700

    enh: sign up agreement text

--- src/routes/entry/Signup.tsx
+++ src/routes/entry/Signup.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/techlance-logo.jpg";
 import './Signup.scss';
 
@@ -8,6 +8,13 @@ const Signup = () => {
 
     const data = new FormData(event.target);
     const payload = Object.fromEntries(data.entries());
+
+    // check if the 
+    if (data.has("agreement")) {
+      console.log("it is checked")
+    } else {
+      console.log("it is not checked")
+    }
 
     console.log("payload: ", JSON.stringify(payload));
   }
@@ -24,8 +31,18 @@ const Signup = () => {
           <input type="text" name="email" id="email" />
           <label htmlFor="password">Password</label>
           <input type="password" name="password" id="password" />
+          <div className="signup__agreement-container">
+            <input type="checkbox" name="agreement" id="agreement" />
+            <label>I agree to the TechLance User Agreement and Privacy Policy.</label>
+          </div>
           <button type="submit">Sign up</button>
         </form>
+        <hr className="signup__hr" />
+        <div className="signup__login">
+          Already have an account? 
+          {" "}
+          <a href="#!">Log in</a>
+        </div>
       </div>
     </div>
   )
