d7d0dd19d0be8d12455a1e88dfa32dd091a3260f
Steve Vinsensius Jo <37164804+stevejo12@users.noreply.github.com>
2022-10-25 17:31:00 +0700

    feat: search list (w/o filter) || refactor color naming

--- src/routes/Search/Search.tsx
+++ src/routes/Search/Search.tsx
@@ -29,8 +29,46 @@ const Search = () => {
 
   return (
     <div className="search">
-      {jobLists.map((jobList) => (
-        <div className="search__jobList">{jobList.company}</div>
+      {jobLists.map((jobList, key) => (
+        <div className="search__jobList" key={key}>
+          <div className="search__jobList-logo">
+            <img src={`${jobList.logo.replace("./", "")}`} alt="company-logo" />
+          </div>
+          <div className="search__jobList-description">
+            <div className="search__jobList-description-job">
+              <span className="search__jobList-description-job-companyName">
+                {jobList.company}
+              </span>
+              {jobList.new && (
+                <span className="search__jobList-description-job-new">
+                  NEW!
+                </span>
+              )}
+              {jobList.featured && (
+                <span className="search__jobList-description-job-featured">
+                  FEATURED
+                </span>
+              )}
+            </div>
+            <div className="search__jobList-description-position">
+              {jobList.position}
+            </div>
+            <div className="search__jobList-description-listingDetail">
+              {jobList.postedAt}
+              {" \u2022 "}
+              {jobList.contract}
+              {" \u2022 "}
+              {jobList.location}
+            </div>
+          </div>
+          <div className="search__jobList-tag">
+            {jobList.languages.map((language, key) => (
+              <span key={key} className="search__jobList-tag-textContainer">
+                {language}
+              </span>
+            ))}
+          </div>
+        </div>
       ))}
       <Link to="/">Back to home</Link>
     </div>
